cc8dabe0409ed33ad2bcd9be936e76117d28e0ab
borfus <mrpeteyb@gmail.com>
2021-01-23 17:25:58 -0600

    Added shader_util.c/h and helloshaders.c to reflect fun color changes.

--- src/shader_source.h
+++ src/shader_source.h
@@ -1,20 +1,30 @@
 #pragma once
 
 const char *vertex_shader_source = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
+    "layout(location = 0) in vec3 aPos;\n"
+    "layout(location = 1) in vec3 aColor;\n"
+
+    "out vec4 vertex_color;\n"
+
     "void main() {\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vertex_color = vec4(aColor, 1.0);\n"
     "}\0";
 
-const char *fragment_shader_source_beige = "#version 330 core\n"
-    "out vec4 FragColor;\n"
+const char *fragment_shader_source_1 = "#version 330 core\n"
+    "uniform vec4 our_color;\n"
+    "out vec4 frag_color;\n"
+
     "void main() {\n"
-    "   FragColor = vec4((226.0f/255.0f), (205.0f/255.0f), (176.0f/255.0f), 1.0f);\n"
+    "   frag_color = our_color;\n"
     "}\0";
 
-const char *fragment_shader_source_purple = "#version 330 core\n"
-    "out vec4 FragColor;\n"
+const char *fragment_shader_source_2 = "#version 330 core\n"
+    "in vec4 vertex_color;\n"
+    "out vec4 frag_color;\n"
+
     "void main() {\n"
-    "   FragColor = vec4((150.0f/255.0f), (120.0f/255.0f), (180.0f/255.0f), 1.0f);\n"
+    "   frag_color = vertex_color;\n"
     "}\0";
+
 
